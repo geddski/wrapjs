fab7c1024598d4eb80d5ecd0f9962ca7fffa4aaf
Dave Geddes <davidcgeddes@gmail.com>
2012-04-11 16:37:54 -0600

    updated to latest require.js

--- examples/js/lib/require.js
+++ examples/js/lib/require.js
@@ -5,6 +5,7 @@
  */
 /*jslint strict: false, plusplus: false, sub: true */
 /*global window, navigator, document, importScripts, jQuery, setTimeout, opera */
+
 var requirejs, require, define;
 (function () {
     //Change this version number for each release.
@@ -137,7 +138,7 @@ var requirejs, require, define;
     if (typeof define !== "undefined") {
         //If a define is already in play via another AMD loader,
         //do not overwrite.
-        // return;
+        return;
     }
 
     if (typeof requirejs !== "undefined") {
